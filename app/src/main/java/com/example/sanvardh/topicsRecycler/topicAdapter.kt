16b0de1ccf67fd9b140e7cf5715e5ae78aef80a1
Vishal <vishalanuragi123@gmail.com>
2021-07-14 17:51:58 +0530

    main recycler done

--- app/src/main/java/com/example/sanvardh/topicsRecycler/topicAdapter.kt
+++ app/src/main/java/com/example/sanvardh/topicsRecycler/topicAdapter.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sanvardh.R
 
@@ -19,6 +21,7 @@ class topicAdapter(
     class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view){
     val img = itemView.findViewById<ImageView>(R.id.topicImage)
     val imgTitle = itemView.findViewById<TextView>(R.id.topicTitle)
+    val card = itemView.findViewById<CardView>(R.id.card1)
     fun bindView(image: topicModel) {
         img.setImageResource(image.imageSrc)
         imgTitle.text = image.title
@@ -32,5 +35,18 @@ override fun getItemCount(): Int = images.size
 
 override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
     holder.bindView(images[position])
+    holder.card.setOnClickListener {
+
+        when (position) {
+            0 -> Toast.makeText(context , "Physics" , Toast.LENGTH_SHORT).show()
+            1 -> Toast.makeText(context , "Biology" , Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(context , "Mechanics" , Toast.LENGTH_SHORT).show()
+            3 -> Toast.makeText(context , "Astronomy" , Toast.LENGTH_SHORT).show()
+            4 -> Toast.makeText(context , "Architecture" , Toast.LENGTH_SHORT).show()
+            5 -> Toast.makeText(context , "Chemistry" , Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
 }
 }
