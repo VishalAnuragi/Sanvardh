951d983fc5a415bc45d6e66cf7cced51f1ad8fe7
Vishal <vishalanuragi123@gmail.com>
2021-07-25 03:22:08 +0530

    UI improved

--- app/src/main/java/com/example/sanvardh/modelsActivity/Models_Activity.java
+++ app/src/main/java/com/example/sanvardh/modelsActivity/Models_Activity.java
@@ -29,14 +29,10 @@ public class Models_Activity extends AppCompatActivity {
         title = findViewById(R.id.titleTxt);
         titleImg = findViewById(R.id.titleImg);
 
-
         Bundle bundle = getIntent().getExtras();
         String topic = bundle.getString("TOPIC");
 
-
-
         createTitles(topic);
-        createMainRecycler();
     }
 
     private void createTitles(String topic) {
@@ -44,55 +40,121 @@ public class Models_Activity extends AppCompatActivity {
         switch(topic) {
             case "PHYSICS" :
                 titleImg.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.physics));
+
+                modelsModel innermodel1[] = {
+
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics),
+                        new modelsModel("Physics", R.drawable.physics)
+                };
+
+                createModelsRecycler(innermodel1);
                 break;
 
             case "BIOLOGY" :
                 titleImg.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.biology));
+
+                modelsModel innermodel2[] = {
+
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology),
+                        new modelsModel("Biology", R.drawable.biology)
+                };
+
+                createModelsRecycler(innermodel2);
                 break;
 
             case "MECHANICS" :
                 titleImg.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.mechanics));
+
+                modelsModel innermodel3[] = {
+
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics),
+                        new modelsModel("Mechanics", R.drawable.mechanics)
+                };
+
+                createModelsRecycler(innermodel3);
                 break;
 
             case "ASTRONOMY" :
                 titleImg.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.astronomy));
+
+                modelsModel innermodel4[] = {
+
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy),
+                        new modelsModel("Astronomy", R.drawable.astronomy)
+                };
+
+                createModelsRecycler(innermodel4);
                 break;
 
             case "ARCHITECTURE" :
                 titleImg.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.architecture));
+
+                modelsModel innermodel5[] = {
+
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture),
+                        new modelsModel("Architecture", R.drawable.architecture)
+                };
+
+                createModelsRecycler(innermodel5);
                 break;
 
             case "CHEMISTRY" :
                 titleImg.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.chemistry));
+
+                modelsModel innermodel6[] = {
+
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry),
+                        new modelsModel("Chemistry", R.drawable.chemistry)
+                };
+
+                createModelsRecycler(innermodel6);
                 break;
         }
     }
 
-    private void createMainRecycler() {
-
-        modelsModel innermodel1[] = {
-
-                new modelsModel("Physics", R.drawable.physics),
-                new modelsModel("Biology", R.drawable.biology),
-                new modelsModel("Mechanics", R.drawable.mechanics),
-                new modelsModel("Astronomy", R.drawable.astronomy),
-                new modelsModel("Architecture", R.drawable.architecture),
-                new modelsModel("Chemistry", R.drawable.chemistry),
-                new modelsModel("Physics", R.drawable.physics),
-                new modelsModel("Biology", R.drawable.biology),
-                new modelsModel("Mechanics", R.drawable.mechanics),
-                new modelsModel("Astronomy", R.drawable.astronomy),
-                new modelsModel("Architecture", R.drawable.architecture),
-                new modelsModel("Chemistry", R.drawable.chemistry),
-
-        };
+    private void createModelsRecycler(modelsModel[] innermodel) {
 
         innerRecycler = findViewById(R.id.innerRecycler);
         innerRecycler.setLayoutManager(new GridLayoutManager( getApplicationContext(), 2 ));
         innerRecycler.setHasFixedSize(true);
 
-
-        modelsAdapter inneradapter = new modelsAdapter(getApplicationContext() , innermodel1);
+        modelsAdapter inneradapter = new modelsAdapter(getApplicationContext() , innermodel);
         innerRecycler.setAdapter(inneradapter);
         innerRecycler.setItemAnimator(new DefaultItemAnimator());
 
